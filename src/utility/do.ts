b255b8380965843ab72d49b782c5d16f0b2eb37f
Poy Chang <poy.chang@seec.com.tw>
2017-11-21 15:34:25 +0800

    add practise of do (named tap at rxjs 5.5) operator

---
+++ src/utility/do.ts
@@ -0,0 +1,26 @@
+// CAUTION!! -------------------------------------------------------------------
+// Starting in version 5.5, do operator renamed to tap.
+// REF: https://github.com/ReactiveX/rxjs/blob/master/doc/lettable-operators.md
+// -----------------------------------------------------------------------------
+
+import {of} from 'rxjs/Observable/of';
+import {map, tap} from 'rxjs/operators';
+
+// tap(nextOrObserver: function, error: function, complete: function):
+// Observable 透明地執行操作，不會影響元素的值，例如執行 console.log()。
+// http://cn.rx.js.org/class/es6/Observable.js~Observable.html#instance-method-do
+
+// 資料來源
+const source$ = of(1, 2, 3, 4, 5);
+const sourceObject$ = of({ name: 'poy', age: 32 },
+                         { name: 'john', age: 28 },
+                         { name: 'tom', age: 25 });
+
+source$.pipe(tap(val => console.log(val))).subscribe();
+
+// 練習
+sourceObject$
+  .pipe(tap(val => console.log(val)),
+        map(val => val.age * 10),
+        tap(val => console.log(val)))
+  .subscribe();
